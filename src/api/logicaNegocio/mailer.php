b9300c5760155e1a9387a09f8d47a93839f6c50e
Diabolo-kun <manuper777@gmail.com>
2025-12-05 19:30:45 +0100

    Update mailer.php

--- src/api/logicaNegocio/mailer.php
+++ src/api/logicaNegocio/mailer.php
@@ -9,9 +9,9 @@
 //  que envían correos (ej. registro/activación de usuario).
 // ------------------------------------------------------------------
 
-require __DIR__ . '/../libs/PHPMailer-7.0.0/src/Exception.php';
-require __DIR__ . '/../libs/PHPMailer-7.0.0/src/PHPMailer.php';
-require __DIR__ . '/../libs/PHPMailer-7.0.0/src/SMTP.php';
+require __DIR__ . '/../../libs/PHPMailer-7.0.0/src/Exception.php';
+require __DIR__ . '/../../libs/PHPMailer-7.0.0/src/PHPMailer.php';
+require __DIR__ . '/../../libs/PHPMailer-7.0.0/src/SMTP.php';
 
 //copia esto en la parte superior del archivo
 // require_once __DIR__ . '/mailer.php';
