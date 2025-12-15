c3cc2a22a96e9fea1788c9e4c37d3cf8e3f6ced3
Diabolo-kun <manuper777@gmail.com>
2025-12-15 12:20:53 +0100

    Update index2.php

--- src/api/index2.php
+++ src/api/index2.php
@@ -16,15 +16,15 @@
 // ini_set('log_errors', 1);
 // ==========================================
 
-// header('Content-Type: application/json');
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
-// header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 
-// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-//     http_response_code(200);
-//     exit;
-// }
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 // -------------------- INCLUSIONES --------------------
 // Conexión a la base de datos
